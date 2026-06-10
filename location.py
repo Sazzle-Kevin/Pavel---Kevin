2bb166b59635f1223008f5b783041b959796856a
kevin <kevinheins@live.de>
2026-06-10 16:34:51 +0200

    Erstellung von location.py, text.py, shop.py und Spiel Einleitung/Charaktererstellung

---
+++ location.py
@@ -0,0 +1,7 @@
+class Location:
+
+    def __init__(self, name, location, enemies=None, rare_encounter=None):
+        self.name = name
+        self.location = location
+        self.enemies = enemies
+        self.rare_encounter = rare_encounter
