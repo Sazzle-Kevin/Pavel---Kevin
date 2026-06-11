d59fe84147ac1cda94b0515e6759375f47a0d5b8
kevin <kevinheins@live.de>
2026-06-11 17:09:37 +0200

    main aufgeräumter, strings, location, world_map update; text enter scroll

--- location.py
+++ location.py
@@ -1,7 +1,54 @@
+from enemy import Enemy
+
+
+def spawn_wolf():
+    return Enemy("Wolf", 0, 0)
+
+
+def spawn_boar():
+    return Enemy("Wildschwein", 0, 0)
+
+
+def spawn_golden_pig():
+    return Enemy("Goldenes Schwein", 0, 0)
+
+
+def spawn_spider():
+    return Enemy("Spinne", 0, 0)
+
+
+def spawn_bat():
+    return Enemy("Fledermaus", 0, 0)
+
+
+def spawn_cave_man():
+    return Enemy("Steinzeitlicher Höhlenmensch", 0, 0)
+
+
+def spawn_vampire():
+    return Enemy("Vampir", 0, 0)
+
+
+def spawn_goblin():
+    return Enemy("Goblin", 0, 0)
+
+
 class Location:
 
-    def __init__(self, name, location, enemies=None, rare_encounter=None):
+    def __init__(self, name, city=False, enemies=None, rare_encounter=None):
         self.name = name
-        self.location = location
+        self.city = city
         self.enemies = enemies
         self.rare_encounter = rare_encounter
+
+
+dorf = Location("Dorf", city=True)
+forrest = Location(
+    "Wald", enemies=[spawn_wolf, spawn_boar], rare_encounter=spawn_golden_pig
+)
+cave = Location(
+    "Höhle", enemies=[spawn_spider, spawn_bat], rare_encounter=spawn_cave_man
+)
+sollum = Location("Sollum", city=True)
+monda = Location("Monda", city=True)
+castle = Location("Finsteres Schloss", enemies=[spawn_vampire, spawn_goblin])
--- main.py
+++ main.py
@@ -1,20 +1,62 @@
 import time
 import strings
+import world_map
+from world_map import current_location
+import location
+from inventory import Inventory
 from player import Player
 from text import slow_print, clear, clear_screen
+
+inventory = Inventory()
+
+
+def run_game():
+    print(" - ".join(key for key in options if options[key] is True), "\n")
+
+    player_input = input().title()
+    if options.get(player_input):
+        options_input[player_input]()
+
+
+def interact_inventory():
+    while True:
+        print(" - ".join(["Öffnen", "Benutzen", "Schließen"]))
+        inventory_input = input()
+        clear_screen()
+        if inventory_input.title() == "Öffnen":
+            print(inventory, "\n")
+        elif inventory_input.title() == "Benutzen":
+            break  ## pass
+        elif inventory_input.title() == "Schließen":
+            break  ## pass
+
 
 options = {
     "Inventar": True,
     "Umgebung": True,
-    "Shop": False,
-    "Weiter reisen": True,
+    "Laden": False,
+    "Karte": True,
 }
 
-## Start ##
-# Einleitung und Namenswahl
+options_input = {
+    "Inventar": interact_inventory,
+    "Umgebung": interact_inventory,  ## pass
+    "Laden": interact_inventory,  ## pass
+    "Karte": interact_inventory,  ## pass
+}
+
+################################################################################
+#                          START - INTRO + NAMESWAHL                           #
+################################################################################
 
 strings.intro()
 
 strings.game_start()
 
-print(" - ".join(key for key in options if options[key] is True))
+
+################################################################################
+#                                     GAME                                     #
+################################################################################
+
+while True:
+    run_game()
